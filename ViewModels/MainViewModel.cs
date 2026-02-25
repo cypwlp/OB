2e51335b33a4acc73ee06ebddc48427d0653ffe4
lw <1511476598@qq.com>
2026-02-25 15:56:04 +0800

    v5.0.5

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -55,6 +55,7 @@ namespace OB.ViewModels
                 new LeftMenuItem { Icon = MaterialIconKind.Home, Title = "首页", ViewName = "Home" },
                 new LeftMenuItem { Icon = MaterialIconKind.Database, Title = "檢測", ViewName = "Detect" },
                 new LeftMenuItem { Icon = MaterialIconKind.ChatProcessing, Title = "流程", ViewName = "Process" },
+                new LeftMenuItem { Icon = MaterialIconKind.ChatProcessing, Title = "導出", ViewName = "Process" },
                 new LeftMenuItem { Icon = MaterialIconKind.CogOutline, Title = "设置", ViewName = "Settings" }
             };
 
